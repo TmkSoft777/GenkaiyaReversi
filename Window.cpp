17dc883eb58918b8ba183fa31a046da003847bbd
777shuang <shuang777gm@gmail.com>
2022-09-08 15:31:34 +0900

    Update Window.cpp

--- Window.cpp
+++ Window.cpp
@@ -1,25 +1,22 @@
+#include <cstdint>
 #include <tchar.h>
 #include <windows.h>
 #include <commctrl.h>
 #include "Board.h"
 
 static HBITMAP ico1_hBitmap, ico2_hBitmap;
-HDC ico1_hBuffer, ico2_hBuffer, wDC, board_tDC, text_tDC;
-HWND m_hWnd, hCommandBar;
+HDC ico1_hBuffer , ico2_hBuffer , wDC , board_tDC , text_tDC;
+HWND m_hWnd;
 HMENU m_hMenu;
-WNDCLASS wndClass;
 HFONT m_hFont;
-HBRUSH m_grayBrush, m_whiteBrush, m_blackBrush, m_greenBrush, m_lightBlueBrush;
+HBRUSH m_grayBrush , m_whiteBrush , m_blackBrush , m_greenBrush , m_lightBlueBrush;
 Board* m_board;
-HBITMAP board_bufferBMP, text_bufferBMP;
-int board_x, board_y, board_w, board_s, text_x, text_y, blackCount , whiteCount, AniTime = 200, DisplayType;
+HBITMAP text_bufferBMP;
+int board_x , board_y , board_w , board_s , text_x , text_y , blackCount , whiteCount , DisplayType;
 //DisplayType: 0:480x360 1:480x800 2:800x480
 //board: x,y:coordinate w:weight s:each space
 auto board = m_board->getBoard();
-void RefreshWindow();
-void RefreshBuffer();
 
-void DrawBuffer(HDC);
 inline void drawText(HDC hDC , const TCHAR *str , RECT rc , UINT format) { DrawText(hDC , str , lstrlen(str) , &rc , format); }
 void drawText(HDC hdc , int i , RECT rc , UINT format)
 {
@@ -27,6 +24,7 @@ void drawText(HDC hdc , int i , RECT rc , UINT format)
 	wsprintf(buf , _T("%d") , i);
 	drawText(hdc , buf , rc , format);
 }
+
 void RefreshBuffer()
 {
 	RECT board_rc = { 0,0,board_w + 1,board_w + 1};
@@ -63,13 +61,16 @@ void RefreshBuffer()
 	DeleteDC(ico1_hBuffer);
 	DeleteDC(ico2_hBuffer);
 	SelectObject(board_tDC, m_lightBlueBrush);
-	if(DisplayType == 0){
+	if(DisplayType == 0)
+	{
 		for (int i = 0; i < 8; ++i)
 		{
 			for (int j = 0; j < 8; ++j)
 			{ if (board[i][j] == BlockStatus::AVAILABLE) { Ellipse(board_tDC, j * 35 + 10, i * 35 + 10, j * 35 + 25, i * 35 + 25); } }
 		}
-	}else{
+	}
+	else
+	{
 		for (int i = 0; i < 8; ++i)
 		{
 			for (int j = 0; j < 8; ++j)
@@ -90,6 +91,12 @@ void RefreshBuffer()
 	drawText(text_tDC, whiteCount, { 65,0,120,50 }, DT_CENTER);
 }
 
+inline void DrawBuffer(HDC wDC)
+{
+	BitBlt(wDC , board_x , board_y , board_w + 1 , board_w + 1 , board_tDC , 0 , 0 , SRCCOPY);
+	BitBlt(wDC,  text_x , text_y, 120, 50, text_tDC, 0, 0, SRCCOPY);
+}
+
 void RefreshWindow()
 {
 	RefreshBuffer();
@@ -102,21 +109,9 @@ void RefreshWindow()
 			m_board->Set();
 			RefreshWindow();
 		};
-		if (whiteCount < blackCount || whiteCount == 0)
-		{
-			SHOW_RESULT(_T("You Win!"));
-			return;
-		}
-		else if (whiteCount == blackCount) // 引き分け
-		{
-			SHOW_RESULT(_T("Draw!"));
-			return;
-		}
-		else if (whiteCount > blackCount || blackCount == 0)
-		{
-			SHOW_RESULT(_T("You Lose...\nGenkaiya..."));
-			return;
-		}
+		if (whiteCount < blackCount || whiteCount == 0) { SHOW_RESULT(_T("You Win!")); }
+		else if (whiteCount == blackCount) { SHOW_RESULT(_T("Draw!")); }
+		else if (whiteCount > blackCount || blackCount == 0) { SHOW_RESULT(_T("You Lose...\nGenkaiya...")); }
 	}
 	else if (m_board->isnoAvailable)
 	{
@@ -124,12 +119,7 @@ void RefreshWindow()
 		m_board->robot();
 		m_board->setAvailable();
 		RefreshWindow();
-		return;
 	}
-}
-inline void DrawBuffer(HDC wDC) {
-	BitBlt(wDC , board_x , board_y , board_w + 1 , board_w + 1 , board_tDC , 0 , 0 , SRCCOPY);
-	BitBlt(wDC,  text_x , text_y, 120, 50, text_tDC, 0, 0, SRCCOPY);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -137,10 +127,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
 	POINTS pos;
 	static LOGFONT font;
-	const auto LOAD_BITMAP = [&](const TCHAR *name){
+	static HBITMAP board_bufferBMP;
+	static unsigned __int16 AniTime = 200;
+	const auto LOAD_BITMAP = [&](const TCHAR *name)
+	{
 		TCHAR path[MAX_PATH];
-		GetModuleFileName(NULL , path , MAX_PATH);
-		*(_tcsrchr(path , _T('\\')) + 1) = _T('\0');
+		GetModuleFileName(NULL , path , MAX_PATH); // 実行可能ファイルのパスを取得
+		*(_tcsrchr(path , _T('\\')) + 1) = _T('\0'); // 実行可能ファイルのディレクトリのみにする
 		lstrcat(path , name);
 		lstrcat(path , _T(".bmp"));
 
@@ -157,6 +150,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		);
 		#endif
 	};
+
+	#ifdef _WIN32_WCE
+	HWND hCommandBar;
+	#endif
+
 	switch (message)
 	{
 		case WM_LBUTTONDOWN:
@@ -209,7 +207,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				case 301:
 					MessageBox (
 						m_hWnd ,
-						TEXT("Genkaiya-chan Reversi\nVersion 2.0\n\n(c)2022 TMK, 777shuang") ,
+						TEXT("Genkaiya Reversi\nVersion : Beta 2.9\n\nCopylight (c) 2022 TMK, 777shuang. All Righits Reserved.") ,
 						TEXT("About") ,
 						MB_OK | MB_ICONASTERISK
 					);
@@ -224,33 +222,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 			break;
 		case WM_CREATE:
+			#ifdef _WIN32_WCE
 			InitCommonControls();
 			hCommandBar = CommandBar_Create(wndClass.hInstance, m_hWnd, 1);
 			if (hCommandBar == 0) {
 				//MessageBox(NULL, TEXT("Cannot create commandbar!"), TEXT("Reversi"), MB_OK | MB_ICONERROR);
 			}
-			CommandBar_InsertMenubarEx(hCommandBar, wndClass.hInstance, _T("WNDMENU"), 0);
-			CommandBar_AddAdornments(hCommandBar, 0, 102);
-			m_hMenu = CommandBar_GetMenu(hCommandBar, 0);
-			if(DisplayType == 0){
-				ico1_hBitmap = LOAD_BITMAP(_T("white_l")) ,ico2_hBitmap = LOAD_BITMAP(_T("black_l"));
-			}else{
-				ico1_hBitmap = LOAD_BITMAP(_T("white")) ,ico2_hBitmap = LOAD_BITMAP(_T("black"));
-			}
-			if (ico1_hBitmap == 0 || ico2_hBitmap == 0) {
-				MessageBox(NULL, TEXT("Cannot load image file(s)!"), TEXT("Reversi"), MB_OK | MB_ICONERROR);
-				UnregisterClass(_T("Reversi"), wndClass.hInstance);
-				DeleteObject(ico1_hBitmap);
-				DeleteObject(ico2_hBitmap);
+			CommandBar_InsertMenubarEx(hCommandBar , ((LPCREATESTRUCT)(lParam))->hInstance , _T("WNDMENU") , 0);
+			CommandBar_AddAdornments(hCommandBar , 0 , 102);
+			m_hMenu = CommandBar_GetMenu(hCommandBar , 0);
+			#endif
+			if(DisplayType == 0) { ico1_hBitmap = LOAD_BITMAP(_T("white_l")) , ico2_hBitmap = LOAD_BITMAP(_T("black_l")); }
+			else { ico1_hBitmap = LOAD_BITMAP(_T("white")) ,ico2_hBitmap = LOAD_BITMAP(_T("black")); }
+			if (!ico1_hBitmap || !ico2_hBitmap)
+			{
+				MessageBox(hWnd , _T("Cannot load image file(s)!") , _T("Reversi"), MB_OK | MB_ICONERROR);
 				ReleaseDC(m_hWnd, wDC);
 				PostQuitMessage(0);
 			}
 
-			m_whiteBrush = CreateSolidBrush(RGB(255, 255, 255));
-			m_blackBrush = CreateSolidBrush(RGB(0, 0, 0));
-			m_grayBrush = CreateSolidBrush(RGB(0, 128, 0));
-			m_greenBrush = CreateSolidBrush(RGB(0, 255, 00));
-			m_lightBlueBrush = CreateSolidBrush(RGB(0xBE, 0xEF, 0xFF));
+			m_whiteBrush = CreateSolidBrush(RGB(255 , 255 , 255));
+			m_blackBrush = CreateSolidBrush(RGB(0 , 0 , 0));
+			m_grayBrush = CreateSolidBrush(RGB(0 , 128 , 0));
+			m_greenBrush = CreateSolidBrush(RGB(0 , 255 , 0));
+			m_lightBlueBrush = CreateSolidBrush(RGB(0xBE , 0xEF , 0xFF));
 
 			m_board = new Board;
 
@@ -284,7 +279,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			DeleteObject(m_grayBrush);
 			DeleteObject(m_lightBlueBrush);
 			DeleteObject(m_greenBrush);
-			UnregisterClass(_T("Reversi"), wndClass.hInstance);
 			DeleteObject(board_tDC);
 			DeleteObject(board_bufferBMP);
 			DeleteObject(board_tDC);
@@ -295,52 +289,51 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			PostQuitMessage(0);
 			break;
 		case WM_PAINT:
-			wDC = BeginPaint(m_hWnd, &ps);
-
+			wDC = BeginPaint(m_hWnd , &ps);
 			DrawBuffer(wDC);
 			EndPaint(m_hWnd, &ps);
 			break;
-		default: return DefWindowProc(hWnd, message, wParam, lParam);
+		default: return DefWindowProc(hWnd , message , wParam , lParam);
 	}
 	return 0;
 }
 
 int main()
 {
-	if(GetDeviceCaps(0, HORZRES) < 800){
-		if(GetDeviceCaps(0, VERTRES) < 480){
+	if(GetDeviceCaps(0 , HORZRES) < 800)
+	{
+		if(GetDeviceCaps(0 , VERTRES) < 480)
+		{
 			DisplayType = 0;
-			board_x = 0;
-			board_y = 0;
-			board_w = 280;
 			board_s = 35;
 			text_x = 300;
 			text_y = 30;
-		}else{
+		}
+		else
+		{
 			DisplayType = 1;
-			board_x = 0;
-			board_y = 0;
-			board_w = 400;
 			board_s = 50;
 			text_x = 30;
 			text_y = 500;
 		}
-	}else{
+	}
+	else
+	{
 		DisplayType = 2;
-		board_x = 0;
-		board_y = 0;
-		board_w = 400;
 		board_s = 50;
 		text_x = 500;
 		text_y = 30;
 	}
-	HINSTANCE hInstance = GetModuleHandle(0);
+	board_x = board_y = 0;
+	board_w = board_s * 8;
+
+	WNDCLASS wndClass;
 	wndClass.style = CS_HREDRAW | CS_VREDRAW;
 
 	wndClass.lpfnWndProc = WndProc;
 	wndClass.cbClsExtra = 0;
 	wndClass.cbWndExtra = 0;
-	wndClass.hInstance = hInstance;
+	wndClass.hInstance = GetModuleHandle(0);
 	wndClass.hIcon = (HICON)LoadImage(0 , _T("reversi.ico") , IMAGE_ICON , 0 , 0 , LR_LOADFROMFILE);
 	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndClass.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 128, 0)); // 背景は緑
@@ -361,20 +354,16 @@ int main()
 	);
 	m_hWnd = CreateWindow (
 		wndClass.lpszClassName ,
-		TEXT("Genkaiya-chan Reversi for Brain V2.9 <BETA>"),
-		WS_OVERLAPPEDWINDOW & ~WS_MINIMIZEBOX,
-		//CW_USEDEFAULT,
-		//CW_USEDEFAULT,
-		//clientRect.right - clientRect.left,
-		//clientRect.bottom - clientRect.top,
-		//WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		m_hMenu,
-		hInstance,
-		NULL);
-	if (m_hWnd == NULL) return -1;
+		TEXT("Genkaiya Reversi Beta 2.9") ,
+		WS_OVERLAPPEDWINDOW & ~WS_MINIMIZEBOX ,
+		CW_USEDEFAULT , CW_USEDEFAULT ,
+		CW_USEDEFAULT , CW_USEDEFAULT ,
+		NULL ,
+		m_hMenu ,
+		wndClass.hInstance ,
+		NULL
+	);
+	if (m_hWnd == NULL) { return -1; }
 
 	ShowWindow(m_hWnd , SW_SHOW);
 	UpdateWindow(m_hWnd);
@@ -386,6 +375,4 @@ int main()
 		DispatchMessage(&msg);
 	}
 	return msg.wParam;
-
-	return 0;
 }
