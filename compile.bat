0556aa7b3b98fac2f135289a22a2889291c8ab77
TmkSoft777 <113012609+TmkSoft777@users.noreply.github.com>
2022-09-08 20:39:00 +0900

    Revert "777shuang"

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 @cls
-arm-mingw32ce-g++  -std=gnu++2a -march=armv5tej -mcpu=arm926ej-s -static -s -o "%~dp0AppMain.exe" "%~dp0window.cpp" "%~dp0board.cpp" "%~dp0win.rc.o" -L%~dp0 -lcommctrl
+arm-mingw32ce-g++  -std=gnu++2a -march=armv5tej -mcpu=arm926ej-s -static -s -o "%~dp0AppMain.exe" "%~dp0window.cpp" "%~dp0board.cpp" "%~dp0win.rc.o" -L "E:\Reversi (1)" -lcommctrl
