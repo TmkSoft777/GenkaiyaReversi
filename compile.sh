5e372e50a13f1bc95ac2101f4c0bb8402e2a69c3
777shuang <shuang777gm@gmail.com>
2022-09-13 18:04:01 +0900

    Rename

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-arm-mingw32ce-windres reversi.rc -o reversi.o
-arm-mingw32ce-g++  -std=gnu++2a -march=armv5tej -mcpu=arm926ej-s -static -s -o AppMain.exe window.cpp board.cpp win.rc.o -lcommctrl
+arm-mingw32ce-windres resource.rc -o resource.o
+arm-mingw32ce-g++  -std=gnu++2a -march=armv5tej -mcpu=arm926ej-s -static -s -o AppMain.exe window.cpp board.cpp resource.o -lcommctrl
+rm resource.o
